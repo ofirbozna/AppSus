daf0602d3c491575b07d456e7d62d3adbe0b29de
nevo0309 <nevo19970309@gmail.com.com>
2025-02-22 19:58:19 +0200

    Style input and notes

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -3,7 +3,13 @@ export function NotePreview({ note, onRemoveNote }) {
     <article className="note-preview">
       <h3>{note.title || 'Untitled'}</h3>
       <p>{note.info.txt}</p>
-      <button onClick={() => onRemoveNote(note.id)}>delete</button>
+      <div className="note-actions">
+        <button onClick={() => onRemoveNote(note.id)}>Delete</button>
+        <button>Color</button>
+        <button>Archive</button>
+        <button>Dup</button>
+        <button>Email</button>
+      </div>
     </article>
   )
 }
