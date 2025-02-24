7a2da1e1ec6abae447bd38ca7dcc8ba87c6dcfd9
ofirbozna <ofirbozn@gmail.com>
2025-02-24 13:51:54 +0200

    Set isRead style

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -4,14 +4,24 @@ import { MailPreview } from "./MailPreview.jsx"
 const { Link } = ReactRouterDOM
 const { useEffect } = React
 export function MailList({ mails, onRemove }) {
-    
+
+    function onSetIsRead(mailId) {
+        mailsService.get(mailId)
+            .then(mail => {
+                mail.isRead = true
+                mailsService.save(mail)
+            })
+
+    }
+
+
 
     return (
         <section className='mails-list'>
             <ul>
                 {mails && mails.map(mail =>
-                    <li className='mail-line' key={mail.id}>
-                        <Link to={`/mail/${mail.id}`} >
+                    <li className={'mail-line' + (mail.isRead  ? ' read' :'') } key={mail.id}>
+                        <Link to={`/mail/${mail.id}`} onClick={() => onSetIsRead(mail.id)}>
                             <MailPreview mail={mail} />
                         </Link>
                         <button className='fa-solid fa-trash' onClick={() => onRemove(mail.id)}></button>
