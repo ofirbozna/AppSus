3e3e022f5471056f76552845508d764547bbadd1
nevo0309 <nevo19970309@gmail.com.com>
2025-02-24 20:37:36 +0200

    Add option on adding new note a background color

--- apps/note/cmps/AddNote.jsx
+++ apps/note/cmps/AddNote.jsx
@@ -1,9 +1,11 @@
 import { noteService } from '../services/note.service.js'
+import { ColorPicker } from './ColorPicker.jsx'
 const { useState } = React
 
 export function AddNote({ onAddNote }) {
   const [title, setTitle] = useState('')
   const [text, setText] = useState('')
+  const [bgColor, setBgColor] = useState('#ffffff1a')
 
   function handleSubmit(ev) {
     ev.preventDefault()
@@ -12,6 +14,7 @@ export function AddNote({ onAddNote }) {
     const newNote = noteService.getDefaultNote()
     newNote.title = title
     newNote.info.txt = text
+    newNote.style = { backgroundColor: bgColor }
 
     noteService
       .saveNote(newNote)
@@ -19,23 +22,21 @@ export function AddNote({ onAddNote }) {
         onAddNote(savedNote)
         setTitle('')
         setText('')
+        setBgColor('#ffffff1a')
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch((err) => console.log(err))
   }
 
   return (
-    <section className="add-note">
+    <section className="add-note" style={{ backgroundColor: bgColor }}>
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Title" value={title} onChange={(ev) => setTitle(ev.target.value)} />
         <textarea placeholder="Take a note..." value={text} onChange={(ev) => setText(ev.target.value)}></textarea>
+
+        <ColorPicker selectedColor={bgColor} onColorSelect={setBgColor} />
+
         <button type="submit">Add Note</button>
       </form>
     </section>
   )
 }
-
-//TODO: ADD success message
-//TODO: ADD other options exp for txt
-//TODO:
--- apps/note/cmps/NoteActions.jsx
+++ apps/note/cmps/NoteActions.jsx
@@ -1,9 +1,13 @@
-export function NoteActions({ note, onRemoveNote, onEdit }) {
+import { ColorPicker } from './ColorPicker.jsx'
+
+export function NoteActions({ note, onRemoveNote, onEdit, onChangeColor }) {
   return (
     <div className="note-actions">
       <button onClick={() => onRemoveNote(note.id)}>Delete</button>
       <button onClick={() => onEdit(note)}>Edit</button>
-      <button>Color</button>
+
+      <div className="color-picker-container">{note.style && <ColorPicker selectedColor={note.style.backgroundColor || '#ffffff1a'} onColorSelect={(color) => onChangeColor(note.id, color)} />}</div>
+
       <button>Archive</button>
       <button>Dup</button>
       <button>Email</button>
