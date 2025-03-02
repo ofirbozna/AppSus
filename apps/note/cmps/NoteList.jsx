8727d835833690719948df127d46c6054e9db139
nevo0309 <nevo19970309@gmail.com.com>
2025-03-02 19:03:37 +0200

    Fix rendering that only show status of note

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,11 +1,13 @@
 import { NotePreview } from './NotePreview.jsx'
+
 export function NoteList({ notes, onMoveToTrash, onMoveToArchive, onEdit, onChangeColor, onPinNote, onDuplicateNote }) {
   if (!notes || !notes.length) return <p>There is no notes...</p>
 
-  const pinnedNotes = notes.filter((note) => note.isPinned)
-  const unPinnedNotes = notes.filter((note) => !note.isPinned)
-  console.log(pinnedNotes)
-  console.log(unPinnedNotes)
+  const filteredNotes = notes.filter((note) => note.status === 'notes')
+
+  // Separate pinned and unpinned notes
+  const pinnedNotes = filteredNotes.filter((note) => note.isPinned)
+  const unPinnedNotes = filteredNotes.filter((note) => !note.isPinned)
 
   return (
     <section className="note-lists-container">
@@ -14,19 +16,21 @@ export function NoteList({ notes, onMoveToTrash, onMoveToArchive, onEdit, onChan
           <h6 className="list-title">Pinned</h6>
           <div className="note-list">
             {pinnedNotes.map((note) => (
-              <NotePreview note={note} onMoveToTrash={onMoveToTrash} onMoveToArchive={onMoveToArchive} onEdit={onEdit} onChangeColor={onChangeColor} onPinNote={onPinNote} key={note.id} onDuplicateNote={onDuplicateNote} />
+              <NotePreview key={note.id} note={note} onMoveToTrash={onMoveToTrash} onMoveToArchive={onMoveToArchive} onEdit={onEdit} onChangeColor={onChangeColor} onPinNote={onPinNote} onDuplicateNote={onDuplicateNote} />
             ))}
           </div>
         </div>
       )}
-      <div className="note-lists-unpin">
-        <h6 className="list-title">{pinnedNotes.length > 0 && 'Others'}</h6>
-        <div className="note-list">
-          {unPinnedNotes.map((note) => (
-            <NotePreview note={note} onMoveToTrash={onMoveToTrash} onMoveToArchive={onMoveToArchive} onEdit={onEdit} onChangeColor={onChangeColor} onPinNote={onPinNote} key={note.id} onDuplicateNote={onDuplicateNote} />
-          ))}
+      {unPinnedNotes.length > 0 && (
+        <div className="note-lists-unpin">
+          <h6 className="list-title">{pinnedNotes.length > 0 ? 'Others' : 'Notes'}</h6>
+          <div className="note-list">
+            {unPinnedNotes.map((note) => (
+              <NotePreview key={note.id} note={note} onMoveToTrash={onMoveToTrash} onMoveToArchive={onMoveToArchive} onEdit={onEdit} onChangeColor={onChangeColor} onPinNote={onPinNote} onDuplicateNote={onDuplicateNote} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </section>
   )
 }
