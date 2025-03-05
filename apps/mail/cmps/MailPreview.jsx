df1ee52631edb6399908e0351402e44bb403dd57
ofirbozna <ofirbozn@gmail.com>
2025-03-06 01:41:08 +0200

    Fix Style in mobile

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -4,7 +4,7 @@ import { MailCompose } from "./MailCompose.jsx"
 export function MailPreview({ mail,filterBy}) {
     return (
         <section className='mail-preview'>
-            <h3>{filterBy.folder=== 'sent' || filterBy.folder==='drafts'? mail.to:mail.from}</h3>
+            <h3>{mail.to !=='ofirNevo@appsus.com'? mail.to:mail.from}</h3>
             <h4>{`${mail.subject}-`}</h4>
             <p>{mail.body}</p>
         </section>
